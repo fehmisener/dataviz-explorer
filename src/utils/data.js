b2f24e4034cda75b5f5841b803b1a9c380978ee2
fehmisener <fehmiisener@gmail.com>
2024-05-08 17:10:36 +0200

    refactor: change csv reader with papaparse

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,23 +1,3 @@
-export const readFileAsync = (file) => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (e) => resolve(e.target.result);
-    reader.onerror = (e) => reject(e);
-    reader.readAsText(file);
-  });
-};
-
-export const parseCSV = (csvContent) => {
-  const lines = csvContent.split(/\r\n|\n/);
-  const data = [];
-  lines.forEach((line) => {
-    const delimiter = line.includes(',') ? ',' : ';';
-    const values = line.split(delimiter);
-    data.push(values);
-  });
-  return data;
-};
-
 export const processHeaderName = (columnName) => {
   const words = columnName
     .split('_')
